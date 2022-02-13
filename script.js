1b5e618a3c336444cb81a1989fa3f5fe936900c4
Rayane Celes <rayane.celes@gmail.com>
2022-02-13 00:04:36 -0300

    CSS Responsivo

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function createCustomElement(type, className = '', text = '') {
   const element = document.createElement(type);
   element.className = className;
-  element.innerText = text;
+  element.innerHTML = text;
   return element;
 }
 
@@ -18,10 +18,9 @@ function addDetailsToItem(item) {
   const keys = Object.keys(item);
   
   keys.forEach(element => {
-    const li = createCustomElement('div', 'detail');
-    let name = element[0].toUpperCase() + element.slice(1);
-    name = name.replace('_', ' ') + ':';
-    li.appendChild(createCustomElement('span', 'detail-name', name));
+    const li = createCustomElement('li', 'detail');
+    const name = `<b>${element[0].toUpperCase()}${element.slice(1)}: </b>`;
+    li.appendChild(createCustomElement('span', element, name));
     const text = createCustomElement('span', 'detail-text');
     text.innerText = item[element];
     li.appendChild(text);
@@ -32,12 +31,12 @@ function addDetailsToItem(item) {
 
 function createItemSection(item) {
   const detail = getItemDetailList(item);
-
-  const container = createCustomElement('div', 'container');
+  const container = createCustomElement('section', 'item-cards');
   const image = createCustomImage(detail.img)
   container.appendChild(image);
-  const div = createCustomElement('div', 'detail_container')
-  container.appendChild(div)
+  // const div = createCustomElement('div', 'detail_container')
+  // const div = createCustomElement('div', 'container')
+  // container.appendChild(div)
   const details = addDetailsToItem(detail);
   container.appendChild(details);
   return container
@@ -47,18 +46,29 @@ function getItemDetailList(item) {
   const detail = {
     title: item.title[0],
     country: item.country[0],
-    providing_institution: item.dataProvider[0],
-    description: item.dcDescription[0],
-    creator: item.dcCreator[0],
+    institution: (item.dataProvider !== undefined) ? item.dataProvider[0] : '',
+    description: (item.dcDescription !== undefined) ? item.dcDescription[0] : '',
+    creator: (item.dcCreator !== undefined) ? item.dcCreator[0] : '',
     img: item.edmPreview[0],
   };
   return detail;
 }
 
-window.onload = async () => {
-  const a = await fetchItem('mona lisa');
-  console.log(getItemDetailList(a[0]));
-  const body = document.querySelector('body')
-  const b = a[0].edmPreview[0]
-  body.appendChild(createItemSection(a[0]))
+async function loadArts () {
+  const query = document.querySelector('#art-search');
+  if (query.value) {
+    const section = document.querySelector('.items');
+    section.innerHTML = '';
+    const data = await fetchItem(query.value);
+    console.log(data);
+    data.forEach((item) => {
+      section.appendChild(createItemSection(item));
+    });
+  }
+  query.value = '';
+}
+
+window.onload = () => {
+  const button = document.querySelector('#btn-finder');
+  button.addEventListener('click', loadArts);
 }
